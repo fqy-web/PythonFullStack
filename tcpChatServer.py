dc2dd6f5e47a230461501960fdc6151dba4c5b4f
fqy-web <18883540090@163.com>
2019-10-25 21:23:22 +0800

    Tcp Server

---
+++ tcpChatServer.py
@@ -0,0 +1,35 @@
+import socket
+import threading    # 使用多线程模块
+
+def main():
+    # 创建socket对象，调用socket构建函数
+    sSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 将socket绑定到指定地址，第一个参数为IP地址，第二个参数为端口号
+    sSocket.bind(('127.0.0.1',5000))
+    # 设置最多连接数量
+    sSocket.listen(10)
+    print('开始启动服务器...')
+
+    while True:
+        # 服务器套接字通过socket的accept方法等待客户请求一个连接
+        cSocket, address = sSocket.accept()
+        print("客户端{0}连接到服务器".format(address))
+        thr1 = threading.Thread(target=recvMsg, args=(cSocket,address))
+        # 启动线程
+        thr1.start()
+
+def recvMsg(cSocket, addr):
+    while True:
+        msg_bytes = cSocket.recv(1024)
+        msg = msg_bytes.decode('utf-8')
+        print('从{0}客户端接收到消息:{1}'.format(addr, msg))
+        if not msg or msg == 'exit':
+            print('关闭客户端{0}连接'.format(addr))
+            cSocket.close()
+            break
+
+        # 将接收到的消息原样返回到客户端
+        cSocket.send(msg_bytes)
+
+if __name__ == '__main__':
+    main()
